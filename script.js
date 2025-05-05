ba050c127f62253ffe157a2d01095235e7de5767
mohib-dbs <mohib.ireland@gmail.com>
2025-05-06 00:15:15 +0100

    3rd exercise - try 2

---
+++ script.js
@@ -0,0 +1,32 @@
+let c=()=>{
+    let i=document.getElementById('in').value
+    if (i>15){
+        alert("Over weight!");
+    }
+}
+let sum=()=>{
+    let a=0;
+    let b=document.getElementById('in2').value;
+    for (let i=1;i<b;i++){
+        if (i%3===0 || i%5===0){
+          a+=i;
+        }
+    }
+    console.log(a);
+    alert("The result is: " + a);
+    document.getElementById('result').textContent = "The sum of multiples of 3 or 5 up to "+ b +" is: " + a;
+}
+const a=document.getElementById('an').value;
+const b=document.getElementById('bn').value;
+const n=document.getElementById('in3').value;
+function sum3() {
+    let d = 0;
+    for (let i = 1; i < n; i++) {
+        if (i % a === 0 || i % b === 0) {
+            d += i;
+        }
+    }
+    console.log(d);
+    alert("The sum of all multiples of " + a + " or " + b + " below " + n + " is: " + d);
+    document.getElementById('result3').textContent = ("The sum of multiples of " + a + " or " + b + " up to " + n + " is: " + d);
+}
