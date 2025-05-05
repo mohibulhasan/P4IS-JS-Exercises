6fd20e9dd0fe44bdbece27addf964b447430d855
mohib-dbs <mohib.ireland@gmail.com>
2025-05-06 00:48:26 +0100

    4th exercise 3rd try

--- script.js
+++ script.js
@@ -38,13 +38,15 @@ function sum4(){
     const a=document.getElementById('a4').value;
     const b=document.getElementById('b4').value;
     const n=[2,4,6,7,8,9,14,23,25,26,27,80,60,9,25];
+    const newarray = []; //for tracking added numbers
     for (let i=0;i<n.length;i++){
-      if (n[i]%a===0 || n[i]%b===0){
+      if ((n[i]%a===0 || n[i]%b===0) && !newarray.includes(n[i])){
         console.log(n[i]);
         d+=n[i];
+        newarray.push(n[i]);
         }
       }
     console.log(d);
-    alert("The sum of all multiples of "+ a +" or "+b+" below "+n+" is: "+d);
-    document.getElementById('result4').textContent = ("The sum of multiples of "+ a +" or "+ b +" up to the array "+ n +" is: " + d);
+    alert("The sum of all unique multiples of " + a + " or " + b + " within the array [" + n.join(", ") + "] is: " + d);
+    document.getElementById('result4').textContent = "The sum of unique multiples of " + a + " or " + b + " within the array [" + n.join(", ") + "] is: " + d;
   }
