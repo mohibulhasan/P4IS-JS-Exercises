f6852e356b79ef59e7c6d8e90dcc7ef5493a770f
mohib-dbs <mohib.ireland@gmail.com>
2025-05-09 14:27:43 +0100

    Correction of Exercise 4

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Exercise 1
 let c = () => {
     let i = document.getElementById('in').value
     if (i > 15) {
@@ -17,7 +18,7 @@ let sum = () => {
     alert("The result is: " + a);
     document.getElementById('result').textContent = "The sum of multiples of 3 or 5 up to " + b + " is: " + a;
 }
-
+// For Exercise 3
 let abn = () => {
     const a = document.getElementById('an').value;
     const b = document.getElementById('bn').value;
@@ -38,47 +39,43 @@ let sum3 = (a,b,n) => {
 
 }
 
-function sum4(){
-    let d=0;
-    const a=document.getElementById('a4').value;
-    const b=document.getElementById('b4').value;
-    const n=[2,4,6,7,8,9,14,23,25,26,27,80,60,9,25];
-    const newarray = []; //for tracking added numbers
-    for (let i=0;i<n.length;i++){
-      if ((n[i]%a===0 || n[i]%b===0) && !newarray.includes(n[i])){
-        console.log(n[i]);
-        d+=n[i];
-        newarray.push(n[i]);
-        }
-      }
-    console.log(d);
-    alert("The sum of all unique multiples of " + a + " or " + b + " within the array [" + n.join(", ") + "] is: " + d);
-    document.getElementById('result4').textContent = "The sum of unique multiples of " + a + " or " + b + " within the array [" + n.join(", ") + "] is: " + d + "(without duplicate)";
-  }
-
-function f4(){
-    let d=0;
+// For Exercise 4 
+let ex4 = () => {
     const a=document.getElementById('a41').value;
     const b=document.getElementById('b41').value;
-    const n = document.getElementById('c4').value;
-    for (let i=1;i<n;i++){
-      if ((i%a===0 || i%b===0)){
+    let inputString = document.getElementById('c4').value;
+    let n = inputString.split(",").map(num => Number(num.trim())); // Convert to an array of numbers
+    let sum = f4(a,b,n);
+    alert("The sum of all multiples of " + a + " or " + b + " below " + n + " is: " + sum);
+    document.getElementById('result4_1').textContent = "The sum of unique multiples of " + a + " or " + b + " within " + n + " is: " + sum;
+}
+
+
+function f4(a,b,n){
+    let d=0;
+    for (let i=1;i<n.length;i++){
+      if ((n[i]%a===0 || n[i]%b===0)){
         console.log(i);
-        d+=i;
+        d+=n[i];
         }
       }
     console.log(d);
-    alert("The sum of all multiples of " + a + " or " + b + " below " + n + " is: " + d);
-    document.getElementById('result4_1').textContent = "The sum of unique multiples of " + a + " or " + b + " within " + n + " is: " + d;
+    return d;
 }
+// For Exercise 5 
 
-function sum5_1(){
-    let d=0;
+let ex5 = () => {
     const a=document.getElementById('a5').value;
     const b=document.getElementById('b5').value;
     const inputString = document.getElementById('arrayInput').value;
     const n = inputString.split(",").map(num => Number(num.trim())); // Convert to an array of numbers
-    const newarray = []; //for tracking added numbers
+    let d =f5(a,b,n);
+    alert("The sum of all unique multiples of " + a + " or " + b + " within the array [" + n.join(", ") + "] is: " + d);
+    document.getElementById('result5_1').textContent = "The sum of unique multiples of " + a + " or " + b + " within the array [" + n.join(", ") + "] is: " + d + "(without duplicate)";
+}
+function f5(a,b,n){
+    let d=0;
+    let newarray = []; //for tracking added numbers
     for (let i=0;i<n.length;i++){
         if ((n[i]%a===0 || n[i]%b===0) && !newarray.includes(n[i])){
             console.log(n[i]);
@@ -87,6 +84,5 @@ function sum5_1(){
             }
         }
     console.log(d);
-    alert("The sum of all unique multiples of " + a + " or " + b + " within the array [" + n.join(", ") + "] is: " + d);
-    document.getElementById('result5_1').textContent = "The sum of unique multiples of " + a + " or " + b + " within the array [" + n.join(", ") + "] is: " + d + "(without duplicate)";
+    return d;    
 }
